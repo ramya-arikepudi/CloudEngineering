bdc674b794ee6422704af4963b3f9dcea66cf34d
ramya-arikepudi <ramya.arikepudi@gmail.com>
2023-05-10 21:30:13 -0400

    Adv Data Types

---
+++ Session3/Adv_data_types.py
@@ -0,0 +1,34 @@
+# my_empty_list = []
+# print(my_empty_list)
+# print(f'This is my empty list data type:{type(my_empty_list)}')
+
+
+letters = ['a','b','c','d',1,2,2,['cat',12.12,'rat']]
+# print(letters)
+#
+# letters.append('hi')
+# print(f'list after appending:{letters}')
+#
+# letters.insert(4,'Ramya')
+# print(f'list after inserting:{letters}')
+
+letters[-1].remove('rat')
+print(f'list after removing:{letters}')
+#
+# letters.remove(2)
+# print(f'list after removing:{letters}')
+#
+# letters.pop(4)
+# print(f'list after pop:{letters}')
+
+# print(f'The value at index -1:{letters[-1]}')
+# print(f'The value at index -2:{letters[-2]}')
+
+#
+# my_dictionary = {}
+# print(f'The data type of an empty dictionary is: {type(my_dictionary)}')
+#
+# my_dictionary = {
+#     "Ramya": 100
+# }
+# print(f'The value of the key Ramya in my dictionary is {my_dictionary["Ramya"]}')
